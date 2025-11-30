e1fcb091dee66fe6c32e54052188860bd7b16960
LightWind1 <amateurdev@outlook.com>
2025-11-30 11:27:19 +0800

    Resolved the issue of cpu_soft_restart being redefined in arch/arm64/kernel/cpu-reset.h by modifying the cpu_soft_restart function calls in arch/arm64/kernel/machine_kexec.c

--- arch/arm64/kernel/cpu-reset.h
+++ arch/arm64/kernel/cpu-reset.h
@@ -16,6 +16,7 @@
 void __cpu_soft_restart(unsigned long el2_switch, unsigned long entry,
 	unsigned long arg0, unsigned long arg1, unsigned long arg2);
 
+#ifndef __ASM_PROCFNS_H
 static inline void __noreturn cpu_soft_restart(unsigned long entry,
 					       unsigned long arg0,
 					       unsigned long arg1,
@@ -33,3 +34,4 @@ static inline void __noreturn cpu_soft_restart(unsigned long entry,
 }
 
 #endif
+#endif /* __ASM_PROCFNS_H */
